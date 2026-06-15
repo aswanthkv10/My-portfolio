d90f1fa1964c66778050129f2619157279c5c571
Aswanth <js7510791770@gmail.com>
2026-06-15 16:46:35 +0530

    Update portfolio components and README

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css',
-})
-export class Footer {
+// @Component({
+//   selector: 'app-footer',
+//   imports: [],
+//   templateUrl: './footer.html',
+//   styleUrl: './footer.css',
+// })
+// export class Footer {
 
-}
+// }
+
+
